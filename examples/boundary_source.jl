37478244d8b3c0d11e3d5889c745028036baf74c
Henrik Wyschka <henrik.wyschka@gmail.com>
2022-11-09 11:09:33 +0100

    Update example codes
    Document, fit to line width and slight changes for walkthrough

--- examples/boundary_source.jl
+++ examples/boundary_source.jl
@@ -1,13 +1,14 @@
 using MinFEM
 
 mesh = import_mesh("../meshes/rounded.msh")
+
 freeBoundary = select_boundaries(mesh, 1003, 1004)
 freeElements = extract_elements(freeBoundary)
 dirichletBoundary = select_boundaries(mesh, 1001, 1002)
 dirichletNodes = extract_nodes(dirichletBoundary)
 
 L = assemble_laplacian(mesh)
-M = assemble_massmatrix_boundary(mesh, boundaryElements=freeElements, order=3)
+M = assemble_massmatrix_boundary(mesh, boundaryElements=freeElements)
 
 f(x) = x[1]^2 + x[2]^2
 s = evaluate_mesh_function(mesh, f, freeBoundary)
